13d3bdfa194a4100cd88d27fc1e68273802b1977
gentooza <joa.cuellar@riseup.net>
2022-03-29 20:02:22 +0200

    updated copyright holders

--- mask2_data.h
+++ mask2_data.h
@@ -2,6 +2,7 @@
 This file is part of PiResiduos.
 
 Copyright 2017-2018, Prointegra SL.
+Copyright 2019-2022, Pixelada S. Coop. And. <info (at) pixelada (dot) org>
 
 PiResiduos is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
