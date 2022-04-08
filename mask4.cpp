3a37d7ec18de191c64535473a9203c75702c0e0a
gentooza <joa.cuellar@riseup.net>
2022-04-08 11:58:48 +0200

    wrong app name in copyright header

--- mask4.cpp
+++ mask4.cpp
@@ -1,20 +1,20 @@
 /*
-This file is part of PixResiduos.
+This file is part of PiResiduos.
 
 Copyright 2017-2018, Prointegra SL.
 Copyright 2021, Pixelada S. Coop. And. <info (at) pixelada (dot) org>
 
-PixResiduos is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+PiResiduos is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-PixResiduos is distributed in the hope that it will 
+PiResiduos is distributed in the hope that it will 
 be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with PixResiduos.  
+along with PiResiduos.  
 If not, see <https://www.gnu.org/licenses/>.
 */
 
