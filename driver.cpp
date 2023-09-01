fc7cb95626da860eefcff907c81b3b4bbd198590
gentooza <joa.cuellar@riseup.net>
2023-09-01 18:11:07 +0200

    optimized and standarized code in class driver

--- driver.cpp
+++ driver.cpp
@@ -2,6 +2,7 @@
 This file is part of PiResiduos.
 
 Copyright 2017-2018, Prointegra SL.
+Copyright 2023 Joaquín Cuéllar <joa (dot) cuellar (at) riseup (dot) net>
 
 PiResiduos is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -24,151 +25,173 @@ If not, see <https://www.gnu.org/licenses/>.
 #include "driver.h"
 
 /*!build by code and database query*/
-driver::driver(long new_code,qtDatabase& my_database)
+driver::driver(long code, qtDatabase& myDatabase)
 {
-  
-  code = new_code;
-  char * sql =NULL;
-  std::vector <std::vector <std::string>> data_return;
-
-  sel_driver_data_by_code(sql,code);
-  my_database.query(NULL,sql);	    
-  data_return = my_database.retData2();
-  
-  if (data_return.size())
-    {
-      _set(data_return.at(0));
-    }
-  else
-    {
-      reset();
-    }
-  return;
-  
+    this->setDriver(code, myDatabase);
+    return;
 }
-/*! build by data from database*/
-driver::driver(std::vector<std::string> database_data)
+/*! Class constructor, build by data from database*/
+driver::driver(std::vector<std::string> databaseData)
 {
-  if(database_data.size())
-    {
-      _set(database_data);
-    }
-  else
-    reset();  
+    this->setDriver(databaseData);
+    return;
 }
 /*!built blank*/
 driver::driver()
 {
-  code = -1;
-  name = "";
-  nif = "";
-  address = "";
-  provence = "";
-  city = "";
-  region = "";
-  zip = 0;
-  nima = 0;
-  num_ins = 0;
-  phone = 0;
-  mail = "";
+    code = -1;
+    name = "";
+    nif = "";
+    address = "";
+    provence = "";
+    city = "";
+    region = "";
+    zip = 0;
+    nima = 0;
+    num_ins = 0;
+    phone = 0;
+    mail = "";
+    return;
 }
 /*!built copied from another*/
 driver::driver(driver * reference)
 {
-  
-  code = reference->get_code();
-  name = reference->get_name();
-  nif = reference->get_nif();
-  address = reference->get_address();
-  provence = reference->get_provence();  
-  city = reference->get_city();
-  region = reference->get_region();
-  zip = reference->get_zip();
-  nima = reference->get_nima();
-  num_ins = reference->get_num_ins();
-  phone = reference->get_phone();
-  mail = reference->get_mail();
-  
+    code = reference->get_code();
+    name = reference->get_name();
+    nif = reference->get_nif();
+    address = reference->get_address();
+    provence = reference->get_provence();  
+    city = reference->get_city();
+    region = reference->get_region();
+    zip = reference->get_zip();
+    nima = reference->get_nima();
+    num_ins = reference->get_num_ins();
+    phone = reference->get_phone();
+    mail = reference->get_mail();
+    return;
 }
 void driver::reset()
 {
-  code = -1;
-  name.clear();
-  nif.clear();
-  address.clear();
-  provence.clear();
-  city.clear();
-  region.clear();
-  zip = 0;
-  nima = 0;
-  num_ins = 0;
-  phone = 0;
-  mail.clear();
+    code = -1;
+    name.clear();
+    nif.clear();
+    address.clear();
+    provence.clear();
+    city.clear();
+    region.clear();
+    zip = 0;
+    nima = 0;
+    num_ins = 0;
+    phone = 0;
+    mail.clear();
+    return;
 }
-
-void driver::_set(std::vector<std::string> database_data)
+/*! build by code and database query */
+void driver::setDriver(long code, qtDatabase& my_database)
 {
-  std::vector <std::string>::iterator iter;
-  int i = 0;
-  
-  for(iter = database_data.begin(); iter != database_data.end(); ++iter)
+    char *sql = NULL;
+
+    sel_driver_data_by_code(sql, code);
+    my_database.query(NULL,sql);	    
+    std::vector <std::vector <std::string>> data_return = my_database.retData2();
+    if(data_return.size())
     {
-      if(i==0) //NAME
-	name = *iter;
-      else if(i==1) //NIF
-	nif = *iter;
-      else if(i==2) //ADDRESS
-	address = *iter;
-      else if(i==3) //PROVENCE
-	provence = *iter;
-      else if(i==4) //CITY
-	city = *iter;
-      else if(i==5) //ZIP CODE
-	try
-	  {
-	    zip = std::stol(*iter);
-	  }
-	catch(const std::invalid_argument& ia)
-	  {
-	    std::cerr << "Invalid argument: " << ia.what() << '\n';
-	    zip = 0;
-	  }
-      else if(i==6) //REGION
-	region = *iter;
-      else if(i==7) //NIMA
-	try
-	  {
-	    nima = std::stol(*iter);
-	  }
-	catch(const std::invalid_argument& ia)
-	  {
-	    std::cerr << "Invalid argument: " << ia.what() << '\n';
-	    nima = 0;
-	  }
-      else if(i==8) //NUM APPLYING
-	try
-	  {
-	    num_ins = std::stol(*iter);
-	  }
-	catch(const std::invalid_argument& ia)
-	  {
-	    std::cerr << "Invalid argument: " << ia.what() << '\n';
-	    num_ins = 0;
-	  }	      
-      else if(i==9) //PHONE
-	try
-	  {
-	    phone = std::stol(*iter);
-	  }
-	catch(const std::invalid_argument& ia)
-	  {
-	    std::cerr << "Invalid argument: " << ia.what() << '\n';
-	    phone = 0;
-	  }
-      else if(i==10) //MAIL
-	mail = *iter;
-      i++;
+        this->setDriver(data_return.at(0));
     }
+    else
+    {
+        std::cerr << "ERROR: driver::setDriver wrong number of fields passed to constructor, NO DATA PASSED" <<  '\n';
+        this->reset();
+    }
+    if(sql != NULL)
+        delete [] sql;
+    return;
+}
+/*! build by data from database*/
+void driver::setDriver(std::vector<std::string> databaseData)
+{
+    if(databaseData.size() >= 15)
+    {
+        std::vector <std::string>::iterator iter;
+        int i = 0;
+        
+        for(iter = databaseData.begin(); iter != databaseData.end(); ++iter)
+        {
+            if(i==0) //NAME
+                name = *iter;
+            else if(i==1) //NIF
+                nif = *iter;
+            else if(i==2) //ADDRESS
+                address = *iter;
+            else if(i==3) //PROVENCE
+                provence = *iter;
+            else if(i==4) //CITY
+                city = *iter;
+            else if(i==5) //ZIP CODE
+            {
+                try
+                {
+                    zip = std::stol(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                    std::cerr << "Invalid argument: " << ia.what() << '\n';
+                    std::cerr << "driver::setDriver In driver POSTAL_CODE field 5 = " << *iter <<  '\n';
+                    zip = 0;
+                }
+            }
+            else if(i==6) //REGION
+                region = *iter;
+            else if(i==7) //NIMA
+            {
+                try
+                {
+                    nima = std::stol(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                    std::cerr << "Invalid argument: " << ia.what() << '\n';
+                    std::cerr << "driver::setDriver In driver NIMA field 7 = " << *iter <<  '\n';
+                    nima = 0;
+                }
+            }
+            else if(i==8) //NUM APPLYING
+            {
+                try
+                {
+                    num_ins = std::stol(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                    std::cerr << "Invalid argument: " << ia.what() << '\n';
+                    std::cerr << "driver::setDriver In driver NUM_INS field 8 = " << *iter <<  '\n';
+                    num_ins = 0;
+                }
+            }
+            else if(i==9) //PHONE
+            {
+                try
+                {
+                    phone = std::stol(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                    std::cerr << "Invalid argument: " << ia.what() << '\n';
+                    std::cerr << "driver::setDriver In driver PHONE field 9 = " << *iter <<  '\n';
+                    phone = 0;
+                }
+            }
+            else if(i==10) //MAIL
+                mail = *iter;
+            i++;
+        }
+    }
+    else
+    {
+        std::cerr << "ERROR: driver::setDriver wrong number of fields passed to constructor, needed 11, received " << databaseData.size() <<  '\n';
+        this->reset();
+    }
+    return;
 }
 
 int driver::is_manually_edited()
