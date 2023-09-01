60f5061e6d73114517a9810fa449187aa52a4ca6
gentooza <joa.cuellar@riseup.net>
2023-09-01 17:36:36 +0200

    optimized and standarized code in class costumer

--- costumer.cpp
+++ costumer.cpp
@@ -2,6 +2,7 @@
 This file is part of PiResiduos.
 
 Copyright 2017-2018, Prointegra SL.
+Copyright 2023 Joaquín Cuéllar <joa (dot) cuellar (at) riseup (dot) net>
 
 PiResiduos is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -22,299 +23,239 @@ If not, see <https://www.gnu.org/licenses/>.
 */
 #include "costumer.h"
 
-costumer::costumer(long cosCode,qtDatabase& myDatabase)
+costumer::costumer(long cosCode, qtDatabase& myDatabase)
 {
-  
-  code = cosCode;
-  char * sql =NULL;
-  std::vector <std::vector <std::string>> dataReturn;
-
-  selCosDataByCode(sql,code);
-  myDatabase.query(NULL,sql);	    
-  dataReturn = myDatabase.retData2();
-  
-  if (dataReturn.size())
-    {
-      std::vector <std::string>::iterator iter;
-      int i = 0;
-      for(iter = dataReturn.at(0).begin(); iter != dataReturn.at(0).end(); ++iter)
-	{
-	  if(i==0) //NOMBRE
-	      name = *iter;
-	  else if(i==1) //nif
-	    nif = *iter;
-	  else if(i==2) //address
-	    address = *iter;
-	  else if(i==3) //provence
-	    provence = *iter;
-	  else if(i==4) //city
-	    city = *iter;
-	  else if(i==5) //postal code
-	    try
-	      {
-		cp = std::stol(*iter);
-	      }
-	    catch(const std::invalid_argument& ia)
-	      {
-		std::cerr << "Invalid argument: " << ia.what() << '\n';
-		cp = 0;
-	      }
-	  else if(i==6) //billing id
-	    try
-	      {
-		billin_code = std::stol(*iter);
-	      }
-	    catch(const std::invalid_argument& ia)
-	      {
-		std::cerr << "Invalid argument: " << ia.what() << '\n';
-		billin_code = 0;
-	      }
-	  else if(i==7) //CBC
-	    try
-	      {
-		cbc = std::stoi(*iter);
-	      }
-	    catch(const std::invalid_argument& ia)
-	      {
-		std::cerr << "Invalid argument: " << ia.what() << '\n';
-		cbc = 0;
-	      }
-	  else if(i==8) //type
-	    try
-	      {
-		type = std::stoi(*iter);
-	      }
-	    catch(const std::invalid_argument& ia)
-	      {
-		std::cerr << "Invalid argument: " << ia.what() << '\n';
-		type = 0;
-	      }
-	  else if(i==9) //region
-	    region = *iter;
-	  else if(i==10) //NIMA
-	    try
-	      {
-		nima = std::stol(*iter);
-	      }
-	    catch(const std::invalid_argument& ia)
-	      {
-		std::cerr << "Invalid argument: " << ia.what() << '\n';
-		nima = 0;
-	      }
-	  else if(i==11) //num ins
-	    try
-	      {
-		num_ins = std::stol(*iter);
-	      }
-	    catch(const std::invalid_argument& ia)
-	      {
-		std::cerr << "Invalid argument: " << ia.what() << '\n';
-		num_ins = 0;
-	      }	      
-	  else if(i==12) //phone
-	    try
-	      {
-		phone = std::stol(*iter);
-	      }
-	    catch(const std::invalid_argument& ia)
-	      {
-		std::cerr << "Invalid argument: " << ia.what() << '\n';
-		phone = 0;
-	      }
-	  else if(i==13) //mail
-	    mail = *iter;
-	  i++;
-	}
-    }
-  else
-    {
-      reset();
-    }
-  return;
-  
+    this->setCostumer(cosCode, myDatabase);
+    return; 
 }
 costumer::costumer(std::vector<std::string> databaseData)
 {
-  std::vector <std::string>::iterator iter;
-  int i = 0;
-  for(iter = databaseData.begin(); iter != databaseData.end(); ++iter)
-    {
-      if(i==0) //code
-	{
-	  try
-	    {
-	      code = std::stol(*iter);
-	    }
-	  catch(const std::invalid_argument& ia)
-	    {
-	      std::cerr << "Invalid argument: " << ia.what() << '\n';
-	      code = 0;
-	    }
-	}
-      if(i==1) //NOMBRE
-	name = *iter;
-      else if(i==2) //nif
-	nif = *iter;
-      else if(i==3) //address
-	address = *iter;
-      else if(i==4) //provence
-	provence = *iter;
-      else if(i==5) //city
-	city = *iter;
-      else if(i==6) //postal code
-	try
-	  {
-	    cp = std::stol(*iter);
-	  }
-	catch(const std::invalid_argument& ia)
-	  {
-	    std::cerr << "Invalid argument: " << ia.what() << '\n';
-	    cp = 0;
-	  }
-      else if(i==7) //billing id
-	try
-	  {
-	    billin_code = std::stol(*iter);
-	  }
-	catch(const std::invalid_argument& ia)
-	  {
-	    std::cerr << "Invalid argument: " << ia.what() << '\n';
-	    billin_code = 0;
-	  }
-      else if(i==8) //CBC
-	try
-	  {
-	    cbc = std::stoi(*iter);
-	  }
-	catch(const std::invalid_argument& ia)
-	  {
-	    std::cerr << "Invalid argument: " << ia.what() << '\n';
-	    cbc = 0;
-	  }
-      else if(i==9) //type
-	try
-	  {
-	    type = std::stoi(*iter);
-	  }
-	catch(const std::invalid_argument& ia)
-	  {
-	    std::cerr << "Invalid argument: " << ia.what() << '\n';
-	    type = 0;
-	  }
-      else if(i==10) //region
-	region = *iter;
-      else if(i==11) //NIMA
-	try
-	  {
-	    nima = std::stol(*iter);
-	  }
-	catch(const std::invalid_argument& ia)
-	  {
-	    std::cerr << "Invalid argument: " << ia.what() << '\n';
-	    nima = 0;
-	  }
-      else if(i==12) //num ins
-	try
-	  {
-	    num_ins = std::stol(*iter);
-	  }
-	catch(const std::invalid_argument& ia)
-	  {
-	    std::cerr << "Invalid argument: " << ia.what() << '\n';
-	    num_ins = 0;
-	  }	      
-      else if(i==13) //phone
-	try
-	  {
-	    phone = std::stol(*iter);
-	  }
-	catch(const std::invalid_argument& ia)
-	  {
-	    std::cerr << "Invalid argument: " << ia.what() << '\n';
-	    phone = 0;
-	  }
-      else if(i==14) //mail
-	mail = *iter;
-      i++;
-    }
-  
+    this->setCostumer(databaseData);
+    return;
 }
 costumer::costumer()
 {
-  code = 0;
-  name = "";
-  nif = "";
-  address = "";
-  provence = "";
-  city = "";
-  region = "";
-  cp = 0;
-  billin_code = 0;
-  cbc = 0;
-  type = 0;
-  nima = 0;
-  num_ins = 0;
-  phone = 0;
-  mail = "";
+    code = 0;
+    name = "";
+    nif = "";
+    address = "";
+    provence = "";
+    city = "";
+    region = "";
+    cp = 0;
+    billin_code = 0;
+    cbc = 0;
+    type = 0;
+    nima = 0;
+    num_ins = 0;
+    phone = 0;
+    mail = "";
+    return;
 }
 costumer::costumer(costumer * reference)
 {
-  
-  code = reference->getCode();
-  try {
-	  name = reference->getName();
-  } catch(...) {
-	  name = "";
-  }
-  nif = reference->getNif();
-  address = reference->getAddress();
-  provence = reference->getProvence();  
-  city = reference->getCity();
-  region = reference->getRegion();
-  cp = reference->getCp();
-  billin_code = reference->getBillin();
-  cbc = reference->getCbc();   
-  type = reference->getType();
-  nima = reference->getNima();
-  num_ins = reference->getNumIns();
-  phone = reference->getPhone();
-  mail = reference->getMail();
-  
+    code = reference->getCode();
+    try {
+        name = reference->getName();
+    } catch(...) {
+        name = "";
+    }
+    nif = reference->getNif();
+    address = reference->getAddress();
+    provence = reference->getProvence();  
+    city = reference->getCity();
+    region = reference->getRegion();
+    cp = reference->getCp();
+    billin_code = reference->getBillin();
+    cbc = reference->getCbc();   
+    type = reference->getType();
+    nima = reference->getNima();
+    num_ins = reference->getNumIns();
+    phone = reference->getPhone();
+    mail = reference->getMail();
+    return;
 }
 void costumer::reset()
 {
-  code = 0;
-  name.clear();
-  nif.clear();
-  address.clear();
-  provence.clear();
-  city.clear();
-  region.clear();
-  cp = 0;
-  billin_code = 0;
-  cbc = 0;
-  type = 0;
-  nima = 0;
-  num_ins = 0;
-  phone = 0;
-  mail.clear();
+    code = 0;
+    name.clear();
+    nif.clear();
+    address.clear();
+    provence.clear();
+    city.clear();
+    region.clear();
+    cp = 0;
+    billin_code = 0;
+    cbc = 0;
+    type = 0;
+    nima = 0;
+    num_ins = 0;
+    phone = 0;
+    mail.clear();
+    return;
 }
 void costumer::setCostumer(long cosCode, qtDatabase& myDatabase)
 {
-  //TODO 
+    char *sql = NULL;
+
+    selCosDataByCode(sql, cosCode);
+    myDatabase.query(NULL, sql);	    
+    std::vector <std::vector <std::string>> dataReturn = myDatabase.retData2(); 
+    if (dataReturn.size())
+    {
+        this->setCostumer(dataReturn.at(0));
+    }
+    else
+    {
+        std::cerr << "ERROR: costumer::setCostumer wrong number of fields passed to constructor, needed 15, NO DATA RECEIVED" <<  '\n';
+        reset();
+    }
+    return;
 }
 void costumer::setCostumer(std::vector<std::string> databaseData)
 {
-  //TODO
+    if(databaseData.size() >= 15)
+    {
+        std::vector <std::string>::iterator iter;
+        int i = 0;
+        for(iter = databaseData.begin(); iter != databaseData.end(); ++iter)
+        {
+            if(i==0) //CODE
+            {
+                try
+                {
+                    code = std::stol(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                std::cerr << "Invalid argument: " << ia.what() << '\n';
+                std::cerr << "costumer::setCostumer In costumer CODE field 0 = " << *iter <<  '\n';
+                code = 0;
+                }
+            }
+            if(i==1) //NAME
+                name = *iter;
+            else if(i==2) //nif
+                nif = *iter;
+            else if(i==3) //address
+                address = *iter;
+            else if(i==4) //provence
+                provence = *iter;
+            else if(i==5) //city
+                city = *iter;
+            else if(i==6) //postal code
+            {
+                try
+                {
+                    cp = std::stol(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                    std::cerr << "Invalid argument: " << ia.what() << '\n';
+                    std::cerr << "costumer::setCostumer In costumer POSTAL_CODE field 6 = " << *iter <<  '\n';
+                    cp = 0;
+                }
+            }
+            else if(i==7) //billing id
+            {
+                try
+                {
+                    billin_code = std::stol(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                    std::cerr << "Invalid argument: " << ia.what() << '\n';
+                    std::cerr << "costumer::setCostumer In costumer BILLING_CODE field 7 = " << *iter <<  '\n';
+                    billin_code = 0;
+                }
+            }
+            else if(i==8) //CBC
+            {
+                try
+                {
+                    cbc = std::stoi(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                    std::cerr << "Invalid argument: " << ia.what() << '\n';
+                    std::cerr << "costumer::setCostumer In costumer CBC field 8 = " << *iter <<  '\n';
+                    cbc = 0;
+                }
+            }
+            else if(i==9) //type
+            {
+                try
+                {
+                    type = std::stoi(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                    std::cerr << "Invalid argument: " << ia.what() << '\n';
+                    std::cerr << "costumer::setCostumer In costumer TYPE field 9 = " << *iter <<  '\n';
+                    type = 0;
+                }
+            }
+            else if(i==10) //region
+                region = *iter;
+            else if(i==11) //NIMA
+            {
+                try
+                {
+                    nima = std::stol(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                    std::cerr << "Invalid argument: " << ia.what() << '\n';
+                    std::cerr << "costumer::setCostumer In costumer NIMA field 11 = " << *iter <<  '\n';
+                    nima = 0;
+                }
+            }
+            else if(i==12) //num ins
+            {
+                try
+                {
+                    num_ins = std::stol(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                    std::cerr << "Invalid argument: " << ia.what() << '\n';
+                    std::cerr << "costumer::setCostumer In costumer NUM_INS field 12 = " << *iter <<  '\n';
+                    num_ins = 0;
+                }
+            }	      
+            else if(i==13) //phone
+            {
+                try
+                {
+                    phone = std::stol(*iter);
+                }
+                catch(const std::invalid_argument& ia)
+                {
+                    std::cerr << "Invalid argument: " << ia.what() << '\n';
+                    std::cerr << "costumer::setCostumer In costumer PHONE field 13 = " << *iter <<  '\n';
+                    phone = 0;
+                }
+            }
+            else if(i==14) //mail
+                mail = *iter;
+            i++;
+            }
+    }
+    else
+    {
+        std::cerr << "ERROR: costumer::setCostumer wrong number of fields passed to constructor, needed 15, received " << databaseData.size() <<  '\n';
+        this->reset();
+    }
+    return;
 }
 
 int costumer::isManuallyEdited()
 {
-  //TODO
-  int ret = 0;
-  if(code == 0)
+    //TODO: to improve?
+    int ret = 0;
+    if(code == 0)
     {
-	ret = 1;
+        ret = 1;
     }
-  return ret;
+    return ret;
 }
--- costumer.h
+++ costumer.h
@@ -2,6 +2,7 @@
 This file is part of PiResiduos.
 
 Copyright 2017-2018, Prointegra SL.
+Copyright 2023 Joaquín Cuéllar <joa (dot) cuellar (at) riseup (dot) net>
 
 PiResiduos is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -34,79 +35,79 @@ If not, see <https://www.gnu.org/licenses/>.
 
 class costumer
 {
- public:
-  costumer(long cosCode, qtDatabase& myDatabase);
-  costumer(std::vector<std::string> databaseData);
-  costumer();
-  costumer(costumer * reference);
-  ~costumer(){};
+    public:
+        costumer(long cosCode, qtDatabase& myDatabase);
+        costumer(std::vector<std::string> databaseData);
+        costumer();
+        costumer(costumer * reference);
+        ~costumer(){};
 
-  void reset();
-  void setCostumer(long cosCode, qtDatabase& myDatabase);
-  void setCostumer(std::vector<std::string> databaseData);
-  int isManuallyEdited();
-  
-  //values
-  long getCode(){return code;};
-  
-  std::string getName(){return name;};
-  void setName(std::string newName){name = newName;};
-  
-  int getType(){return type;};
-  void setType(int newType){((newType>0 && newType <7)?type = newType:type=type);};
-  
-  std::string getNif(){return nif;};
-  void setNif(std::string newNif){nif = newNif;};
-  
-  std::string getAddress(){return address;};
-  void setAddress(std::string newAddress){address = newAddress;};
-  
-  std::string getProvence(){return provence;};
-  void setProvence(std::string newProvence){provence = newProvence;};
-  
-  std::string getCity(){return city;};
-  void setCity(std::string newCity){city = newCity;};
-  
-  std::string getRegion(){return region;};
-  void setRegion(std::string newRegion){region = newRegion; };
-  
-  long getCp(){return cp;}
-  void setCp(long newZip){cp = newZip; };
+        void reset();
+        void setCostumer(long cosCode, qtDatabase& myDatabase);
+        void setCostumer(std::vector<std::string> databaseData);
+        int isManuallyEdited();
+        
+        //values
+        long getCode(){return code;};
+        
+        std::string getName(){return name;};
+        void setName(std::string newName){name = newName;};
+        
+        int getType(){return type;};
+        void setType(int newType){((newType>0 && newType <7)?type = newType:type=type);};
+        
+        std::string getNif(){return nif;};
+        void setNif(std::string newNif){nif = newNif;};
+        
+        std::string getAddress(){return address;};
+        void setAddress(std::string newAddress){address = newAddress;};
+        
+        std::string getProvence(){return provence;};
+        void setProvence(std::string newProvence){provence = newProvence;};
+        
+        std::string getCity(){return city;};
+        void setCity(std::string newCity){city = newCity;};
+        
+        std::string getRegion(){return region;};
+        void setRegion(std::string newRegion){region = newRegion; };
+        
+        long getCp(){return cp;}
+        void setCp(long newZip){cp = newZip; };
 
-  long getBillin(){return billin_code;}
-  void setBillin(long newBillinCode){billin_code = newBillinCode; };
+        long getBillin(){return billin_code;}
+        void setBillin(long newBillinCode){billin_code = newBillinCode; };
 
-  int getCbc(){return cbc;};
-  void setCbc(int newCbc){cbc=newCbc;};
-  
-  long getNima(){return nima;};
-  void setNima(long newNima){nima = newNima;};
-  
-  long getNumIns(){return num_ins;};
-  void setNumIns(long newNumIns){num_ins = newNumIns;};
-  
-  long getPhone(){return phone;};
-  void setPhone(long newPhone){phone = newPhone;};
-  
-  std::string getMail(){return mail;};
-  void setMail(std::string newMail){mail=newMail;};
+        int getCbc(){return cbc;};
+        void setCbc(int newCbc){cbc=newCbc;};
+        
+        long getNima(){return nima;};
+        void setNima(long newNima){nima = newNima;};
+        
+        long getNumIns(){return num_ins;};
+        void setNumIns(long newNumIns){num_ins = newNumIns;};
+        
+        long getPhone(){return phone;};
+        void setPhone(long newPhone){phone = newPhone;};
+        
+        std::string getMail(){return mail;};
+        void setMail(std::string newMail){mail=newMail;};
     
- private:
-  long code;
-  std::string name;
-  std::string nif;
-  std::string address;
-  std::string provence;
-  std::string city;
-  std::string region;
-  long cp;
-  long billin_code;
-  int cbc;
-  int type;
-  long nima;
-  long num_ins;
-  long phone;
-  std::string mail;
+    private:
+        long code;
+        std::string name;
+        std::string nif;
+        std::string address;
+        std::string provence;
+        std::string city;
+        std::string region;
+        long cp;
+        long billin_code;
+        int cbc;
+        int type;
+        long nima;
+        long num_ins;
+        long phone;
+        std::string mail;
 };
 
 #endif
