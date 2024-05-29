a5580e2628c9a7fc2cda97ecb85bdc010ce9a065
gentooza <joa.cuellar@riseup.net>
2024-05-29 10:21:10 +0200

    formatted code a little bit in mask2 GUI

--- src/gui/mask2_actualiza.h
+++ src/gui/mask2_actualiza.h
@@ -81,255 +81,300 @@ static int actualizaForm(PARAM *p,DATA* d,int estado)
 //acciones
 static int actualizaEstado(PARAM *p, DATA *d)
 {
-  int ret = -1;
-  char error[128];
-  if(d->estadoActual != d->estadoFuturo)
+    int ret = -1;
+    char error[128];
+    if(d->estadoActual != d->estadoFuturo)
     {
-      switch(d->estadoFuturo)
-	{
-	case -1:
-	  ret = 0;
-	  break;
-	case 0:
-	  ret = 0;
-	  break;
-	case 1:
-	  {
-
-	  try
+        switch(d->estadoFuturo)
 	    {
-		d->camPos = std::stoi(d->selectedCam.substr(10,1));
-	    }
-	  catch(...)
-	    {
-	      d->camPos = 0;
-	    }
-
-	    if(d->camPos >0 && d->camPos <= 4)
-	      d->camPos--;
-	    else
-	      d->camPos=0;
+	        case -1:
+            case 0:
+            {
+	            ret = 0;
+	            break;
+            }
+	        case 1:
+	        {
+	            try
+	            {
+		            d->camPos = std::stoi(d->selectedCam.substr(10,1));
+	            }
+	            catch(...)
+	            {
+	                d->camPos = 0;
+	            }
+	            if(d->camPos >0 && d->camPos <= 4)
+	                d->camPos--;
+	            else
+	                d->camPos=0;
+	            pvSetText(p,EDITIP,cameras.at(d->camPos).ip.c_str());
+	            d->editedCamIp=cameras.at(d->camPos).ip.c_str();
 	    
-	    pvSetText(p,EDITIP,cameras.at(d->camPos).ip.c_str());
-	    d->editedCamIp=cameras.at(d->camPos).ip.c_str();
+	            char port[10];
+	            sprintf(port,"%d",cameras.at(d->camPos).port);
+	            pvSetText(p,EDITPORT,port);
+	            d->editedCamPort=port;
 	    
-	    char port[10];
-	    sprintf(port,"%d",cameras.at(d->camPos).port);
-	    pvSetText(p,EDITPORT,port);
-	    d->editedCamPort=port;
-	    
-	    char timeout[10];
-	    sprintf(timeout,"%d",cameras.at(d->camPos).timeout);
-	    pvSetText(p,EDITTIMEOUT,timeout);
-	    d->editedCamTimeout=timeout;
-	    
-	    ///relays
-	    if((cameras.at(d->camPos).relayA == cameras.at(d->camPos).relayB) && (cameras.at(d->camPos).relayA != -1))
-	      {
-		cameras.at(d->camPos).relayB = -1;
-		miIni->setCamRelayB(d->camPos+1,cameras.at(d->camPos).relayB);
-	      }
-	    switch(cameras.at(d->camPos).relayA)
-	      {
-	      case(1):
-		d->chsenCamRelay = cameras.at(d->camPos).relayA;
-		pvSetCurrentItem(p,COMBORELAY,0);
-		break;
-	      case(2):
-		d->chsenCamRelay = cameras.at(d->camPos).relayA;
-		pvSetCurrentItem(p,COMBORELAY,1);
-		break;
-	      case(3):
-		d->chsenCamRelay = cameras.at(d->camPos).relayA;
-		pvSetCurrentItem(p,COMBORELAY,2);
-		break;
-	      case(4):
-		d->chsenCamRelay = cameras.at(d->camPos).relayA;
-		pvSetCurrentItem(p,COMBORELAY,3);
-		break;
-	      default:
-		d->chsenCamRelay = cameras.at(d->camPos).relayA = -1;
-		pvSetCurrentItem(p,COMBORELAY,4);
-		miIni->setCamRelayA(d->camPos+1,cameras.at(d->camPos).relayA);
-		break;
-	      }
-	    switch(cameras.at(d->camPos).relayB)
-	      {
-	      case(1):
-		d->chsenCamRelayB = cameras.at(d->camPos).relayB;
-		pvSetCurrentItem(p,COMBORELAYB,0);
-		break;
-	      case(2):
-		d->chsenCamRelayB = cameras.at(d->camPos).relayB;
-		pvSetCurrentItem(p,COMBORELAYB,1);
-		break;
-	      case(3):
-		d->chsenCamRelayB = cameras.at(d->camPos).relayB;
-		pvSetCurrentItem(p,COMBORELAYB,2);
-		break;
-	      case(4):
-		d->chsenCamRelayB = cameras.at(d->camPos).relayB;
-		pvSetCurrentItem(p,COMBORELAYB,3);
-		break;
-	      default:
-		d->chsenCamRelayB = cameras.at(d->camPos).relayB = -1;
-		pvSetCurrentItem(p,COMBORELAYB,4);
-		miIni->setCamRelayB(d->camPos+1,cameras.at(d->camPos).relayB);
-		break;
-	      }
-	    ret = 0;
-	  
-	  break;
-	  }
-	case 2:
-	  {
+	            char timeout[10];
+	            sprintf(timeout,"%d",cameras.at(d->camPos).timeout);
+	            pvSetText(p,EDITTIMEOUT,timeout);
+	            d->editedCamTimeout=timeout;
 
-	  try
-	    {
-	      d->basPos = std::stoi(d->selectedBas.substr(10,1));
-	    }
-	  catch(...)
-	    {
-	      d->basPos = 0;
-	    }
-
-	   
-	    if(d->basPos >0 && d->basPos <= 4)
-	      d->basPos--;
-	    else
-	      d->basPos=0;
-
-	    //cargamos los datos de la báscula
-	    ///puerto
-	    pvSetText(p,EDITTTY,basculas.at(d->basPos).port.c_str());
-	    d->editedBasPort=basculas.at(d->basPos).port;
-	    ///baudios
-	    switch(basculas.at(d->basPos).bauds)
-	      {
-	      case(4800):
-		d->chsenBasBauds = basculas.at(d->basPos).bauds;
-		pvSetCurrentItem(p,COMBOBAUDS,0);
-		break;
-	      case(9600):
-		d->chsenBasBauds = basculas.at(d->basPos).bauds;
-		pvSetCurrentItem(p,COMBOBAUDS,1);
-		break;
-	      case(19200):
-		d->chsenBasBauds = basculas.at(d->basPos).bauds;
-		pvSetCurrentItem(p,COMBOBAUDS,2);
-		break;
-	      default:
-		d->chsenBasBauds = basculas.at(d->basPos).bauds = 9600;	    
-		pvSetCurrentItem(p,COMBOBAUDS,1);
-		miIni->setBasBauds(d->basPos+1,basculas.at(d->basPos).bauds);
-		break;
-	      }
-	    ///timeout
-	    char editbox[32];
-	    sprintf(editbox,"%d",basculas.at(d->basPos).timeout);
-	    pvSetText(p,EDITBASTIMEOUT,editbox);
-	    d->editedBasTimeout = basculas.at(d->basPos).timeout;
-	    ///comando
-	    pvSetText(p,EDITBASASK,basculas.at(d->basPos).ask.c_str());
-	    d->editedBasAsk=basculas.at(d->basPos).ask;
-	    ///paridad
-	    switch(basculas.at(d->basPos).parity)
-	      {
-	      case(0):
-		d->chsenBasParity = basculas.at(d->basPos).parity;
-		pvSetCurrentItem(p,COMBOPARITY,0);
-		break;
-	      case(1):
-		d->chsenBasParity = basculas.at(d->basPos).parity;
-		pvSetCurrentItem(p,COMBOPARITY,1);
-		break;
-	      case(2):
-		d->chsenBasParity = basculas.at(d->basPos).parity;
-		pvSetCurrentItem(p,COMBOPARITY,2);
-		break;
-	      default:		
-		d->chsenBasParity = basculas.at(d->basPos).parity = 0;
-		pvSetCurrentItem(p,COMBOPARITY,0);
-		miIni->setBasParity(d->basPos+1,basculas.at(d->basPos).parity);
-		break;
-	      }
-	    ///bits de parada
-	    switch(basculas.at(d->basPos).stopBits)
-	      {
-	      case(0):
-		d->chsenBasStopBits = basculas.at(d->basPos).stopBits;
-		pvSetCurrentItem(p,COMBOSTOP,0);
-		break;
-	      case(1):
-		d->chsenBasStopBits = basculas.at(d->basPos).stopBits;
-		pvSetCurrentItem(p,COMBOSTOP,1);
-		break;
-	      case(2):
-		d->chsenBasStopBits = basculas.at(d->basPos).stopBits;
-		pvSetCurrentItem(p,COMBOSTOP,2);
-		break;
-	      default:		
-		d->chsenBasStopBits = basculas.at(d->basPos).stopBits = 0;
-		pvSetCurrentItem(p,COMBOSTOP,0);
-		miIni->setBasStopBits(d->basPos+1,basculas.at(d->basPos).stopBits);
-		break;
-	      }
-	    ///Flow control
-	    switch(basculas.at(d->basPos).ctrl)
-	      {
-	      case(0):
-		d->chsenBasFlow = basculas.at(d->basPos).ctrl;
-		pvSetCurrentItem(p,COMBOFLUJO,0);
-		break;
-	      case(1):
-		d->chsenBasFlow = basculas.at(d->basPos).ctrl;
-		pvSetCurrentItem(p,COMBOFLUJO,1);
-		break;
-	      case(2):
-		d->chsenBasFlow = basculas.at(d->basPos).ctrl;
-		pvSetCurrentItem(p,COMBOFLUJO,2);
-		break;
-	      default:		
-		d->chsenBasFlow = basculas.at(d->basPos).ctrl = 0;
-		pvSetCurrentItem(p,COMBOFLUJO,0);
-		miIni->setBasCtrl(d->basPos+1,basculas.at(d->basPos).ctrl);
-		break;
-	      }
-	    //
-	    ret = 0;
-	  
-	  break;
-	  }
-		case 3: // selected a printer position
-		{
-			if(!strcmp(d->selectedPrinter.c_str(), "impresora de DIs")) 
-				miIni->retPrinterId(d->actualPrinter);
-			if(!strcmp(d->selectedPrinter.c_str(), "impresora de tickets"))
-				miIni->retTicketPrinterId(d->actualPrinter);
-  			if(!d->actualPrinter.empty())
-			{
-				pvSetCurrentItem(p, COMBOACTUALIMP, 0);
-				std::vector <std::string> strPrinters = retPrinters();
-				for(int i = 0; i < strPrinters.size(); ++i)
-				{
-					if(!strcmp(strPrinters[i].c_str(), d->actualPrinter.c_str()))
-						pvSetCurrentItem(p, COMBOACTUALIMP, i+1);	
-				}
-			}
-			else 
-			{
-				pvSetCurrentItem(p, COMBOACTUALIMP, 0);
-			}
-		}
-	default:
-	  ret = -1;
-	  break;
-	}
-      d->estadoActual = d->estadoFuturo;
-      actualizaForm(p,d,d->estadoActual);
+	            ///relays
+	            if((cameras.at(d->camPos).relayA == cameras.at(d->camPos).relayB) && (cameras.at(d->camPos).relayA != -1))
+	            {
+		            cameras.at(d->camPos).relayB = -1;
+		            miIni->setCamRelayB(d->camPos+1,cameras.at(d->camPos).relayB);
+	            }
+	            switch(cameras.at(d->camPos).relayA)
+	            {
+	                case(1):
+                    {
+		                d->chsenCamRelay = cameras.at(d->camPos).relayA;
+		                pvSetCurrentItem(p,COMBORELAY,0);
+		                break;
+                    }
+	                case(2):
+                    {
+		                d->chsenCamRelay = cameras.at(d->camPos).relayA;
+		                pvSetCurrentItem(p,COMBORELAY,1);
+		                break;
+                    }
+	                case(3):
+                    {
+		                d->chsenCamRelay = cameras.at(d->camPos).relayA;
+		                pvSetCurrentItem(p,COMBORELAY,2);
+		                break;
+                    }
+	                case(4):
+                    {
+		                d->chsenCamRelay = cameras.at(d->camPos).relayA;
+		                pvSetCurrentItem(p,COMBORELAY,3);
+		                break;
+                    }
+	                default:
+                    {
+		                d->chsenCamRelay = cameras.at(d->camPos).relayA = -1;
+		                pvSetCurrentItem(p,COMBORELAY,4);
+		                miIni->setCamRelayA(d->camPos+1,cameras.at(d->camPos).relayA);
+		                break;
+	                }
+                }
+	            switch(cameras.at(d->camPos).relayB)
+	            {
+	                case(1):
+                    {
+		                d->chsenCamRelayB = cameras.at(d->camPos).relayB;
+		                pvSetCurrentItem(p,COMBORELAYB,0);
+		                break;
+                    }
+	                case(2):
+                    {
+		                d->chsenCamRelayB = cameras.at(d->camPos).relayB;
+		                pvSetCurrentItem(p,COMBORELAYB,1);
+		                break;
+                    }
+	                case(3):
+                    {
+		                d->chsenCamRelayB = cameras.at(d->camPos).relayB;
+		                pvSetCurrentItem(p,COMBORELAYB,2);
+		                break;
+                    }
+	                case(4):
+                    {
+		                d->chsenCamRelayB = cameras.at(d->camPos).relayB;
+		                pvSetCurrentItem(p,COMBORELAYB,3);
+		                break;
+                    }
+	                default:
+                    {
+		                d->chsenCamRelayB = cameras.at(d->camPos).relayB = -1;
+		                pvSetCurrentItem(p,COMBORELAYB,4);
+		                miIni->setCamRelayB(d->camPos+1,cameras.at(d->camPos).relayB);
+		                break;
+	                }
+                }
+	            ret = 0;
+	            break;
+	        }
+    	    case 2:
+	        {
+	            try
+	            {
+	                d->basPos = std::stoi(d->selectedBas.substr(10,1));
+	            }
+	            catch(...)
+	            {
+	                d->basPos = 0;
+	            }
+	            if(d->basPos >0 && d->basPos <= 4)
+	                d->basPos--;
+	            else
+	                d->basPos=0;
+	            //cargamos los datos de la báscula
+	            ///puerto
+	            pvSetText(p,EDITTTY,basculas.at(d->basPos).port.c_str());
+	            d->editedBasPort=basculas.at(d->basPos).port;
+	            ///baudios
+	            switch(basculas.at(d->basPos).bauds)
+	            {
+	                case(4800):
+                    {
+		                d->chsenBasBauds = basculas.at(d->basPos).bauds;
+		                pvSetCurrentItem(p,COMBOBAUDS,0);
+		                break;
+                    }
+	                case(9600):
+                    {
+		                d->chsenBasBauds = basculas.at(d->basPos).bauds;
+		                pvSetCurrentItem(p,COMBOBAUDS,1);
+		                break;
+                    }
+	                case(19200):
+                    {
+		                d->chsenBasBauds = basculas.at(d->basPos).bauds;
+		                pvSetCurrentItem(p,COMBOBAUDS,2);
+		                break;
+                    }
+	                default:
+                    {
+		                d->chsenBasBauds = basculas.at(d->basPos).bauds = 9600;	    
+		                pvSetCurrentItem(p,COMBOBAUDS,1);
+		                miIni->setBasBauds(d->basPos+1,basculas.at(d->basPos).bauds);
+		                break;
+                    }
+	            }
+	            ///timeout
+	            char editbox[32];
+	            sprintf(editbox,"%d",basculas.at(d->basPos).timeout);
+	            pvSetText(p,EDITBASTIMEOUT,editbox);
+	            d->editedBasTimeout = basculas.at(d->basPos).timeout;
+	            ///comando
+	            pvSetText(p,EDITBASASK,basculas.at(d->basPos).ask.c_str());
+	            d->editedBasAsk=basculas.at(d->basPos).ask;
+	            ///paridad
+	            switch(basculas.at(d->basPos).parity)
+	            {
+	                case(0):
+                    {
+		                d->chsenBasParity = basculas.at(d->basPos).parity;
+		                pvSetCurrentItem(p,COMBOPARITY,0);
+		                break;
+                    }
+	                case(1):
+                    {
+		                d->chsenBasParity = basculas.at(d->basPos).parity;
+		                pvSetCurrentItem(p,COMBOPARITY,1);
+		                break;
+                    }
+	                case(2):
+                    {
+		                d->chsenBasParity = basculas.at(d->basPos).parity;
+		                pvSetCurrentItem(p,COMBOPARITY,2);
+		                break;
+                    }
+	                default:
+                    {
+		                d->chsenBasParity = basculas.at(d->basPos).parity = 0;
+		                pvSetCurrentItem(p,COMBOPARITY,0);
+		                miIni->setBasParity(d->basPos+1,basculas.at(d->basPos).parity);
+		                break;
+                    }
+	            }
+	            ///bits de parada
+	            switch(basculas.at(d->basPos).stopBits)
+	            {
+	                case(0):
+                    {
+		                d->chsenBasStopBits = basculas.at(d->basPos).stopBits;
+		                pvSetCurrentItem(p,COMBOSTOP,0);
+		                break;
+                    }
+	                case(1):
+                    {
+		                d->chsenBasStopBits = basculas.at(d->basPos).stopBits;
+		                pvSetCurrentItem(p,COMBOSTOP,1);
+		                break;
+                    }
+	                case(2):
+                    {
+		                d->chsenBasStopBits = basculas.at(d->basPos).stopBits;
+		                pvSetCurrentItem(p,COMBOSTOP,2);
+		                break;
+                    }
+	                default:
+                    {
+		                d->chsenBasStopBits = basculas.at(d->basPos).stopBits = 0;
+		                pvSetCurrentItem(p,COMBOSTOP,0);
+		                miIni->setBasStopBits(d->basPos+1,basculas.at(d->basPos).stopBits);
+		                break;
+                    }
+	            }
+	            ///Flow control
+	            switch(basculas.at(d->basPos).ctrl)
+	            {
+	                case(0):
+                    {
+		                d->chsenBasFlow = basculas.at(d->basPos).ctrl;
+		                pvSetCurrentItem(p,COMBOFLUJO,0);
+		                break;
+                    }
+	                case(1):
+                    {
+		                d->chsenBasFlow = basculas.at(d->basPos).ctrl;
+		                pvSetCurrentItem(p,COMBOFLUJO,1);
+		                break;
+                    }
+	                case(2):
+                    {
+		                d->chsenBasFlow = basculas.at(d->basPos).ctrl;
+		                pvSetCurrentItem(p,COMBOFLUJO,2);
+		                break;
+                    }
+	                default:
+                    {
+		                d->chsenBasFlow = basculas.at(d->basPos).ctrl = 0;
+		                pvSetCurrentItem(p,COMBOFLUJO,0);
+		                miIni->setBasCtrl(d->basPos+1,basculas.at(d->basPos).ctrl);
+		                break;
+                    }
+	            }
+	            //
+	            ret = 0;
+	            break;
+	        }
+		    case 3: // selected a printer position
+		    {
+			    if(!strcmp(d->selectedPrinter.c_str(), "impresora de DIs")) 
+				    miIni->retPrinterId(d->actualPrinter);
+			    if(!strcmp(d->selectedPrinter.c_str(), "impresora de tickets"))
+				    miIni->retTicketPrinterId(d->actualPrinter);
+  			    if(!d->actualPrinter.empty())
+			    {
+				    pvSetCurrentItem(p, COMBOACTUALIMP, 0);
+				    std::vector <std::string> strPrinters = retPrinters();
+				    for(int i = 0; i < strPrinters.size(); ++i)
+				    {
+					    if(!strcmp(strPrinters[i].c_str(), d->actualPrinter.c_str()))
+						    pvSetCurrentItem(p, COMBOACTUALIMP, i+1);	
+				    }
+			    }
+			    else 
+			    {
+				    pvSetCurrentItem(p, COMBOACTUALIMP, 0);
+			    }
+                break;
+		    }
+	        default:
+            {
+	            ret = -1;
+	            break;
+	        }
+        }
+        d->estadoActual = d->estadoFuturo;
+        actualizaForm(p,d,d->estadoActual);
     }
-
-  return ret;
+    return ret;
 }
 
 
